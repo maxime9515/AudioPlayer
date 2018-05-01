8a6cc3d1617d8aa2aae673db84c451e1fccbb6f5
maxime9515 <m.lichkovsky@gmail.com>
2018-05-01 13:51:10 +0300

    0.1

---
+++ app/src/main/java/com/maxim/lychkovski/myaudioplayer/PlaybackStatus.java
@@ -0,0 +1,7 @@
+package com.maxim.lychkovski.myaudioplayer;
+
+
+public enum PlaybackStatus {
+    PLAYING,
+    PAUSED
+}
---
+++ app/src/main/java/com/maxim/lychkovski/myaudioplayer/onItemClickListener.java
@@ -0,0 +1,9 @@
+package com.maxim.lychkovski.myaudioplayer;
+
+import android.view.View;
+
+
+public interface onItemClickListener {
+
+    public void onClick(View view, int index);
+}
